ead84b518c93a1ab557322b73f0b2961aecde15f
bbena99 <bbena99@gmail.com>
2026-05-31 19:35:20 -0500

    Made first room layout. Made helper function printRoom() to test the room layouts.

--- Backend/main.go
+++ Backend/main.go
@@ -6,4 +6,29 @@ import (
 
 func main() {
 	fmt.Println("Hello World!")
+	printRoom(Beginner())
+}
+
+func printRoom(grid [][]int) {
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[i]); j++ {
+			if grid[i][j] == 1 {
+				fmt.Print("███")
+			} else {
+				fmt.Print(" □ ")
+			}
+		}
+		fmt.Println()
+	}
+}
+
+func Beginner() [][]int {
+	return [][]int{
+		{0, 1, 1, 1, 1, 0},
+		{1, 1, 1, 1, 1, 1},
+		{1, 1, 0, 0, 1, 1},
+		{1, 1, 0, 0, 1, 1},
+		{1, 1, 1, 1, 1, 1},
+		{0, 1, 1, 1, 1, 0},
+	}
 }
